280bd0472800016d57bbf51169761120c8b08ec2
camillehe1992 <camille.he@outlook.com>
2024-06-26 10:59:08 +0800

    chore: update command

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,7 +71,7 @@ resource "null_resource" "dist" {
   }
 
   provisioner "local-exec" {
-    command = "cd ${pwd} && rm -r ${var.website_root} && mkdir ${var.website_root} && cp *.html ${var.website_root}/"
+    command = "cd .. && rm -r ${var.website_root} && mkdir ${var.website_root} && cp *.html ${var.website_root}/"
   }
 }
 
